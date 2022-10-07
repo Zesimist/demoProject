3ab961eeeffc753860eea40481923fb5c015075a
Zeeshan <zesimist@yahoo.com>
2022-10-07 17:19:52 +0530

    Updates

---
+++ precache-manifest.bbfe3b8a6cc62524267c7a4c72785352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25f29234507125a8a20b",
+    "url": "/demoProject/static/css/main.99826db5.chunk.css"
+  },
+  {
+    "revision": "25f29234507125a8a20b",
+    "url": "/demoProject/static/js/main.25f29234.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/demoProject/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/demoProject/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "1b76c8b0ccb2787bc92d",
+    "url": "/demoProject/static/js/runtime~main.1b76c8b0.js"
+  },
+  {
+    "revision": "dd78125f2e35647aa8c78e46372a3d30",
+    "url": "/demoProject/index.html"
+  }
+];
